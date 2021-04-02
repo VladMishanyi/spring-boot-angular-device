70177bfbbece570dd138369c6ade1e5a6a68a6ac
KIP-PC99 <vlad.mishaniy@gmail.com>
2021-04-02 08:44:06 +0300

    add tasks layout

---
+++ src/main/java/com/vk/springbootangulardevice/chain/ChainModbus.java
@@ -0,0 +1,45 @@
+package com.vk.springbootangulardevice.chain;
+
+import com.vk.springbootangulardevice.modbus.entity.ModbusBodyQuery;
+import com.vk.springbootangulardevice.tasks.TaskMB110_1TD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@Component
+@ComponentScan(basePackages = {"com.vk.springbootangulardevice.tasks"})
+public class ChainModbus extends Thread{
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    public static Queue<ModbusBodyQuery> modbusBodyQueryQueue = new LinkedList<>();
+
+    private final TaskMB110_1TD taskMB110_1TD;
+
+    @Autowired
+    public ChainModbus(final TaskMB110_1TD taskMB110_1TD){
+        this.taskMB110_1TD = taskMB110_1TD;
+        this.start();
+    }
+
+    @Override
+    public void run(){
+        while (!this.isInterrupted()){
+            try {
+
+                taskMB110_1TD.readModbusAndWriteToTable();
+
+                this.sleep(1000);
+            }catch (InterruptedException e){
+                String message = e.getMessage();
+                LOGGER.error("Interrupted"+this.getClass()+"thread --"+message);
+                System.out.println("Interrupted"+this.getClass()+"thread --"+message);
+            }
+        }
+    }
+}
